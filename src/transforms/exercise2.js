d3c52994932e37d16b1931f40a502d070444ccb5
Miguel Cardoso <miguel.cardoso@typeform.com>
2020-09-30 10:00:21 +0100

    transformation exercises

--- src/transforms/exercise2.js
+++ src/transforms/exercise2.js
@@ -1,3 +1,14 @@
-module.exports = function(fileInfo, api) {
-  // TODO
+const { assignmentExpression } = require("jscodeshift")
+
+module.exports = function(file, api) {
+  const j = api.jscodeshift;
+
+  return j(file.source)
+  .find(j.ExpressionStatement)
+  .forEach(path => {
+    if (path.value.expression.arguments.length < 2) {
+      j(path).remove()
+    }
+  })
+  .toSource();
 }
--- src/transforms/exercise3.js
+++ src/transforms/exercise3.js
@@ -1,3 +1,19 @@
-module.exports = function(fileInfo, api) {
-  // TODO
+module.exports = function(file, api) {
+  const j = api.jscodeshift;
+
+  let res = j(file.source)
+  .find(j.ImportDeclaration, { specifiers: [ { local: { name: 'PropTypes' } }] })
+  .forEach((path, index) => {
+    j(path).remove()
+  })
+  .toSource();
+  res = j(res)
+  .find(j.ExpressionStatement).forEach(p => j(p).remove())
+  .toSource();
+  res = j(res)
+  .find(j.ArrowFunctionExpression)
+  .find(j.ObjectPattern, { properties: [{ value: { name: 'badBoi' } }] })
+  .forEach(p => j(p).remove())
+  .toSource();
+  return res
 }
